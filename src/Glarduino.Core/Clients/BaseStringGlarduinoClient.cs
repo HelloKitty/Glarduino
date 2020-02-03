0667553d407c6b8b4056840b69381b0aed5901f0
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 07:43:40 -0600

    Added BaseStringGlarduinoClient

---
+++ src/Glarduino.Core/Clients/BaseStringGlarduinoClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glarduino
+{
+	public abstract class BaseStringGlarduinoClient : BaseGlarduinoClient<string>
+	{
+		protected BaseStringGlarduinoClient(ArduinoPortConnectionInfo connectionInfo, 
+			IMessageDeserializerStrategy<string> messageDeserializer, 
+			IMessageDispatchingStrategy<string> messageDispatcher) 
+			: base(connectionInfo, messageDeserializer, messageDispatcher)
+		{
+
+		}
+	}
+}
