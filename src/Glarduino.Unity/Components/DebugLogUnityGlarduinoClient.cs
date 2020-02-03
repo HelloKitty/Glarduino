89d09ab762cb83ab22bf822b8dda059bace2f31b
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 07:52:33 -0600

    Added DebugLogUnityGlarduinoClient

---
+++ src/Glarduino.Unity/Components/DebugLogUnityGlarduinoClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Glarduino
+{
+	public sealed class DebugLogGlarduinoClient : MonoBehaviour
+	{
+		[SerializeField]
+		[Tooltip("Port name with which the SerialPort object will be created.")]
+		private string portName = "COM3";
+
+		[SerializeField]
+		[Tooltip("Baud rate that the serial device is using to transmit data.")]
+		private int baudRate = 9600;
+
+		private async Task Start()
+		{
+			UnityStringGlarduinoClient client = new UnityStringGlarduinoClient(new ArduinoPortConnectionInfo(portName, baudRate), new StringMessageDeserializerStrategy(), new DebugLogStringMessageDispatchingStrategy());
+			await client.StartListeningAsync();
+		}
+	}
+}
