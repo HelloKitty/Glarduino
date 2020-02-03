e9ee9840ece2cefda171328a28e3c32e45bd4e74
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 06:36:11 -0600

    Added IConnectionEventsSubscribable

---
+++ src/Glarduino.Core/Events/IConnectionEventsSubscribable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glarduino
+{
+	public interface IConnectionEventsSubscribable
+	{
+		event EventHandler OnClientConnected;
+
+		event EventHandler OnClientDisconnected;
+	}
+}
