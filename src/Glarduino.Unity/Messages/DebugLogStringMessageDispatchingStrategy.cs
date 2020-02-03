e90c179b20f52b003ca32475a72f0e9f85afe0a8
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 07:49:30 -0600

    Added DebugLogStringMessageDispatchingStrategy

---
+++ src/Glarduino.Unity/Messages/DebugLogStringMessageDispatchingStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Glarduino
+{
+	public sealed class DebugLogStringMessageDispatchingStrategy : IMessageDispatchingStrategy<string>
+	{
+		public Task DispatchMessageAsync(string message)
+		{
+			Debug.Log(message);
+			return Task.CompletedTask;
+		}
+	}
+}
