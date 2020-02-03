fea456beeaec84bdfe24aaf4172972661c6c0c00
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 06:38:53 -0600

    Added ConnectionEvents

---
+++ src/Glarduino.Core/Events/ConnectionEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glarduino
+{
+	public sealed class ConnectionEvents : IConnectionEventsSubscribable
+	{
+		public event EventHandler OnClientConnected;
+
+		public event EventHandler OnClientDisconnected;
+
+		public void InvokeClientConnected()
+		{
+			OnClientConnected?.Invoke(this, EventArgs.Empty);
+		}
+
+		public void InvokeClientDisconnected()
+		{
+			OnClientDisconnected?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
