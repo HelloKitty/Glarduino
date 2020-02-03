5857aa01eee2164b75d4b0504802d6438f9cccc5
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 08:05:09 -0600

    Added better stop handling for the Debug unity3D client

--- src/Glarduino.Unity/Components/DebugLogUnityGlarduinoClient.cs
+++ src/Glarduino.Unity/Components/DebugLogUnityGlarduinoClient.cs
@@ -16,15 +16,33 @@ namespace Glarduino
 		[Tooltip("Baud rate that the serial device is using to transmit data.")]
 		private int baudRate = 9600;
 
+		private IDisposable CurrentClient;
+
 		private async Task Start()
 		{
 			UnityStringGlarduinoClient client = new UnityStringGlarduinoClient(new ArduinoPortConnectionInfo(portName, baudRate), new StringMessageDeserializerStrategy(), new DebugLogStringMessageDispatchingStrategy());
 
+			CurrentClient = client;
 			client.ConnectionEvents.OnClientConnected += (sender, args) => Debug.Log($"Port: {portName} connected.");
 			client.ConnectionEvents.OnClientDisconnected += (sender, args) => Debug.Log($"Port: {portName} disconnected.");
 
 			await client.ConnectAsync();
 			await client.StartListeningAsync();
+		}
+
+		void OnDisable()
+		{
+			//When disabled we should just dispose.
+			CurrentClient?.Dispose();
+		}
+
+		/// <summary>
+		/// See: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnApplicationQuit.html
+		/// </summary>
+		void OnApplicationQuit()
+		{
+			//When disabled we should just dispose.
+			CurrentClient?.Dispose();
 		}
 	}
 }
