4c37eb5d6816efb4d8c0f30f21bd17b7ab045018
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 07:42:41 -0600

    Added StringMessageDeserializerStrategy

---
+++ src/Glarduino.Core/Messages/StringMessageDeserializerStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Glarduino
+{
+	public sealed class StringMessageDeserializerStrategy : IMessageDeserializerStrategy<string>
+	{
+		public Task<string> ReadMessageAsync(SerialPort serialPort, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return serialPort.ReadLineAsync();
+		}
+	}
+}
