25b4a27b1b2008ee82c281a6566ed0935b5d6e0a
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 06:33:50 -0600

    Added IClientConnectable

---
+++ src/Glarduino.Core/Clients/IClientConnectable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glarduino
+{
+	public interface IClientConnectable
+	{
+		/// <summary>
+		/// Opens the underlying Arduino port.
+		/// </summary>
+		/// <returns></returns>
+		Task<bool> ConnectAsync();
+	}
+}
