48442a695748840adffcec17c2f8b3ba55668148
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 07:40:33 -0600

    Added StartListeningAsync to BaseGlarduinoClient

--- src/Glarduino.Core/Clients/BaseGlarduinoClient.cs
+++ src/Glarduino.Core/Clients/BaseGlarduinoClient.cs
@@ -9,7 +9,7 @@ namespace Glarduino
 	/// <summary>
 	/// Base type for any Glarduino Arduino connected client.
 	/// </summary>
-	public abstract class BaseGlarduinoClient : IClientConnectable
+	public abstract class BaseGlarduinoClient<TMessageType> : IClientConnectable, IClientListenable
 	{
 		private ConnectionEvents _ConnectionEvents { get; }
 
@@ -27,16 +27,30 @@ namespace Glarduino
 		/// <summary>
 		/// Indicates if the client is connected.
 		/// </summary>
-		public bool isConnected => InternallyManagedPort.IsOpen;
+		public virtual bool isConnected => InternallyManagedPort.IsOpen;
 
 		/// <summary>
 		/// Container for subscribable connection events for the client.
 		/// </summary>
 		public IConnectionEventsSubscribable ConnectionEvents => _ConnectionEvents;
 
-		protected BaseGlarduinoClient(ArduinoPortConnectionInfo connectionInfo)
+		/// <summary>
+		/// Strategy for deserializing messages from the serial port.
+		/// </summary>
+		private IMessageDeserializerStrategy<TMessageType> MessageDeserializer { get; }
+
+		/// <summary>
+		/// Strategy for dispatching messages.
+		/// </summary>
+		private IMessageDispatchingStrategy<TMessageType> MessageDispatcher { get; }
+
+		protected BaseGlarduinoClient(ArduinoPortConnectionInfo connectionInfo, 
+			IMessageDeserializerStrategy<TMessageType> messageDeserializer, 
+			IMessageDispatchingStrategy<TMessageType> messageDispatcher)
 		{
 			ConnectionInfo = connectionInfo ?? throw new ArgumentNullException(nameof(connectionInfo));
+			MessageDeserializer = messageDeserializer ?? throw new ArgumentNullException(nameof(messageDeserializer));
+			MessageDispatcher = messageDispatcher ?? throw new ArgumentNullException(nameof(messageDispatcher));
 			InternallyManagedPort = new SerialPort(connectionInfo.PortName, connectionInfo.BaudRate);
 
 			_ConnectionEvents = new ConnectionEvents();
@@ -62,6 +76,25 @@ namespace Glarduino
 				_ConnectionEvents.InvokeClientConnected();
 
 			return Task.FromResult(InternallyManagedPort.IsOpen);
+		}
+
+		public async Task StartListeningAsync()
+		{
+			if(!isConnected)
+				throw new InvalidOperationException($"Cannot start listening with {nameof(StartListeningAsync)} when internal {nameof(SerialPort)} {InternallyManagedPort} is not connected/open.");
+
+			while (isConnected)
+			{
+				//TODO: Handle cancellation tokens better.
+				TMessageType message = await MessageDeserializer.ReadMessageAsync(InternallyManagedPort);
+
+				//A message was recieved and deserialized as the strategy implemented.
+				//Therefore we should assume we have a valid message and then pass it to the message dispatching strategy.
+				await MessageDispatcher.DispatchMessageAsync(message);
+			}
+
+			//TODO: Should we assume disconnection just because listening stopped?
+			_ConnectionEvents.InvokeClientDisconnected();
 		}
 	}
 }
