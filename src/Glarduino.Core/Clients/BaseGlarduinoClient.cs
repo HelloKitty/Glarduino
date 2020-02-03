6e96da776fb2217b0caf4be184b885fd176dc843
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 06:34:04 -0600

    Added BaseGlarduinoClient

---
+++ src/Glarduino.Core/Clients/BaseGlarduinoClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glarduino
+{
+	/// <summary>
+	/// Base type for any Glarduino Arduino connected client.
+	/// </summary>
+	public abstract class BaseGlarduinoClient : IClientConnectable
+	{
+		/// <summary>
+		/// The internally managed <see cref="SerialPort"/> that represents
+		/// the potentially connected port to the Aurdino device.
+		/// </summary>
+		protected SerialPort InternallyManagedPort { get; } = null;
+
+		/// <summary>
+		/// The connection info used for the <see cref="InternallyManagedPort"/>.
+		/// </summary>
+		protected ArduinoPortConnectionInfo ConnectionInfo { get; }
+
+		/// <summary>
+		/// Indicates if the client is connected.
+		/// </summary>
+		public bool isConnected => InternallyManagedPort.IsOpen;
+
+		protected BaseGlarduinoClient(ArduinoPortConnectionInfo connectionInfo)
+		{
+			ConnectionInfo = connectionInfo ?? throw new ArgumentNullException(nameof(connectionInfo));
+			InternallyManagedPort = new SerialPort(connectionInfo.PortName, connectionInfo.BaudRate);
+		}
+
+		public Task<bool> ConnectAsync()
+		{
+			//Copy ardity
+			InternallyManagedPort.ReadTimeout = ConnectionInfo.ReadTimeout;
+			InternallyManagedPort.WriteTimeout = ConnectionInfo.WriteTimeout;
+
+			InternallyManagedPort.Open();
+
+			return Task.FromResult(InternallyManagedPort.IsOpen);
+		}
+	}
+}
