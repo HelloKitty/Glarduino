81f02a6e945a772c1e3cb5e98fa5bc02db074c60
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 07:39:03 -0600

    Added IMessageDispatchingStrategy

---
+++ src/Glarduino.Core/Messages/IMessageDispatchingStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Glarduino
+{
+	/// <summary>
+	/// Contract for types that implement message dispatching.
+	/// </summary>
+	/// <typeparam name="TMessageType">The message type.</typeparam>
+	public interface IMessageDispatchingStrategy<in TMessageType>
+	{
+		/// <summary>
+		/// Dispatches the provided <see cref="message"/> with the implemented
+		/// strategy.
+		/// </summary>
+		/// <param name="message">The message object.</param>
+		/// <returns>Awaitable for when the dispatching process has completed.</returns>
+		Task DispatchMessageAsync(TMessageType message);
+	}
+}
