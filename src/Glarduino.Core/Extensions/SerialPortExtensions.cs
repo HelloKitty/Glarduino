779a7f2a2dba221236b16b7b73dfa3e00ff0a129
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 05:44:53 -0600

    Added SerialPortExtensions

---
+++ src/Glarduino.Core/Extensions/SerialPortExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glarduino
+{
+	public static class SerialPortExtensions
+	{
+		public static async Task ReadAsync(this SerialPort serialPort, byte[] buffer, int offset, int count)
+		{
+			var bytesRead = 0;
+
+			while (bytesRead < count)
+			{
+				int readBytes = await serialPort.BaseStream.ReadAsync(buffer, bytesRead + offset, count - bytesRead);
+				bytesRead += readBytes;
+			}
+		}
+
+		public static async Task<byte[]> ReadAsync(this SerialPort serialPort, int count)
+		{
+			var buffer = new byte[count];
+			await serialPort.ReadAsync(buffer, 0, count);
+			return buffer;
+		}
+	}
+}
