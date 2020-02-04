2815b6d8271e1afbd3311b302857e8976cfb8081
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 20:14:40 -0600

    Extended ICommunicationPort

--- src/Glarduino.Core/Adapters/ICommunicationPort.cs
+++ src/Glarduino.Core/Adapters/ICommunicationPort.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Glarduino
 {
 	public interface ICommunicationPort : IDisposable
 	{
-		bool IsOpen { get; set; }
+		bool IsOpen { get; }
 
 		int ReadTimeout { get; set; }
 
 		int WriteTimeout { get; set; }
+
+		Encoding Encoding { get; }
+
+		Stream BaseStream { get; }
 
 		void Open();
 
