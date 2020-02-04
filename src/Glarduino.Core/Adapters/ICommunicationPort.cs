d4596027dc8ad73fc48738fe9ed88b481b2223a9
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 20:08:29 -0600

    Added ICommunicationPort

---
+++ src/Glarduino.Core/Adapters/ICommunicationPort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glarduino
+{
+	public interface ICommunicationPort : IDisposable
+	{
+		bool IsOpen { get; set; }
+
+		int ReadTimeout { get; set; }
+
+		int WriteTimeout { get; set; }
+
+		void Open();
+
+		void Close();
+	}
+}
