fe8600ca13c347316054fcfca08b5b67536feb55
HelloKitty <andrew.blakely@ymail.com>
2020-02-03 23:02:13 -0600

    Added BaseUnityGlarduinoAdapterClient

---
+++ src/Glarduino.Unity/Components/BaseUnityGlarduinoAdapterClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glarduino
+{
+	public abstract class BaseUnityGlarduinoAdapterClient : MonoBehaviour
+	{
+		[SerializeField]
+		[Tooltip("Port name with which the SerialPort object will be created.")]
+		private string portName = "COM3";
+
+		[SerializeField]
+		[Tooltip("Baud rate that the serial device is using to transmit data.")]
+		private int baudRate = 9600;
+
+		protected int BaudRate => baudRate;
+
+		protected string PortName => portName;
+
+		protected abstract IDisposable CurrentClient { get; }
+
+		void OnDisable()
+		{
+			//When disabled we should just dispose.
+			CurrentClient?.Dispose();
+		}
+
+		/// <summary>
+		/// See: https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnApplicationQuit.html
+		/// </summary>
+		void OnApplicationQuit()
+		{
+			//When disabled we should just dispose.
+			CurrentClient?.Dispose();
+		}
+	}
+}
