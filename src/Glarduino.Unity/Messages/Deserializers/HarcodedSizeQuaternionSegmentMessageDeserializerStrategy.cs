c12934e7f6db7656076322d464ac516fadd9e89a
HelloKitty <andrew.blakely@ymail.com>
2020-02-04 21:52:57 -0600

    Added HarcodedSizeQuaternionSegmentMessageDeserializerStrategy

---
+++ src/Glarduino.Unity/Messages/Deserializers/HarcodedSizeQuaternionSegmentMessageDeserializerStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Glarduino
+{
+	public class HarcodedSizeQuaternionSegmentMessageDeserializerStrategy : QuaternionSegmentMessageDeserializerStrategy
+	{
+		private int HarcodedSize { get; }
+
+		public HarcodedSizeQuaternionSegmentMessageDeserializerStrategy(int harcodedSize)
+		{
+			if (harcodedSize < 0) throw new ArgumentOutOfRangeException(nameof(harcodedSize));
+
+			HarcodedSize = harcodedSize;
+		}
+
+		protected override Task<byte> GetQuaternionCountAsync(ICommunicationPort serialPort, CancellationToken cancellationToken)
+		{
+			return Task.FromResult((byte)HarcodedSize);
+		}
+	}
+}
