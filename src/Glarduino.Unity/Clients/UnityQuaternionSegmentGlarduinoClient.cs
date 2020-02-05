70e88b8e2182be80c3de07d2564c00ea15cb2bcd
HelloKitty <andrew.blakely@ymail.com>
2020-02-04 18:53:21 -0600

    Added UnityQuaternionSegmentGlarduinoClient

---
+++ src/Glarduino.Unity/Clients/UnityQuaternionSegmentGlarduinoClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Glarduino
+{
+	public sealed class UnityQuaternionGlarduinoClient : BaseGlarduinoClient<ArraySegment<Quaternion>>
+	{
+		private bool unityDisposed = false;
+
+		public UnityQuaternionGlarduinoClient(ArduinoPortConnectionInfo connectionInfo, 
+			IMessageDeserializerStrategy<ArraySegment<Quaternion>> messageDeserializer, 
+			IMessageDispatchingStrategy<ArraySegment<Quaternion>> messageDispatcher) 
+			: base(connectionInfo, messageDeserializer, messageDispatcher)
+		{
+
+		}
+
+		public UnityQuaternionGlarduinoClient(ArduinoPortConnectionInfo connectionInfo, 
+			IMessageDeserializerStrategy<ArraySegment<Quaternion>> messageDeserializer, 
+			IMessageDispatchingStrategy<ArraySegment<Quaternion>> messageDispatcher, 
+			ICommunicationPort comPort) 
+			: base(connectionInfo, messageDeserializer, messageDispatcher, comPort)
+		{
+
+		}
+
+		//Override to only indicate being connected if the application is in playmode too.
+		public override bool isConnected => base.isConnected && !unityDisposed;
+
+		public override void Dispose()
+		{
+			unityDisposed = true;
+			base.Dispose();
+		}
+	}
+}
