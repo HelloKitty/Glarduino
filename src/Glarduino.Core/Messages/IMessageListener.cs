28a1f962c6dd3eeb3317e997cce978a804b83ca3
HelloKitty <andrew.blakely@ymail.com>
2020-02-04 19:59:24 -0600

    Added IMessageListener

---
+++ src/Glarduino.Core/Messages/IMessageListener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glarduino
+{
+	/// <summary>
+	/// Conctract for types that can consume or listen for
+	/// messages produced by the Glardiuno client.
+	/// </summary>
+	/// <typeparam name="TMessageType">The type of the message.</typeparam>
+	public interface IMessageListener<in TMessageType>
+	{
+		/// <summary>
+		/// Called when a message is recieved or processed.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		void OnMessage(TMessageType message);
+	}
+}
